27ad32fb40909e960b3a893d06679e3d6f0260bd
RYAN MILLS <rmills95@uw.edu>
2016-12-05 20:55:47 -0800

    changes

--- server.R
+++ server.R
@@ -1,10 +1,17 @@
-shinyServer(function(input, output) {
+library(shiny)
+library(dplyr)
+library(plotly)
+shinyServer(function(input, output, session) {
   
   #Ryan's widgets -- not complete
   dataInputChart <- reactive({
-    switch(input$plotSelect,
-           "Chart1" = "PromoChart",
-           "Chart2" = "DrugSales",
-           "Chart3" = "DrugPrescriptions")
+    switch(input$plot,
+           "promoChart" = promoChart,
+           "drugSales" = drugSales,
+           "drugPrescriptions" = drugPrescriptions)
   })
-)}
+  output$value <- renderPrint({ input$radio })
+  output$chart <- renderPlot({
+    plot(cars, type=input$plotType)
+  })
+})
--- ui.r
+++ ui.r
@@ -1,8 +1,10 @@
 library(shiny)
 library(dplyr)
 library(plotly)
+library(markdown)
 
-shinyUI(navbarPage('Info Final Project',
+shinyUI(
+  navbarPage('Info Final Project',
     tabPanel('Hospital map',
       sidebarLayout(
         sidebarPanel(
@@ -16,18 +18,25 @@ shinyUI(navbarPage('Info Final Project',
     tabPanel('Drug Chart',
       sidebarLayout(
         sidebarPanel(
-          selectInput('plotSelect', label = "Select Graph:", 
-                      choices = list("Promoted Drugs" = 'Chart1', "Overall Drug Prescriptions" = 'Chart2',
-                                     "Overall Drug Sales" = 'Chart3'),
-                      selected = "Chart1"
-          ),
-          br(),
-          conditionalPanel(condition = "input$plotSelect == 'Chart2'",
-                           checkboxInput("compareToData", label = "Compare to Drug Promotions", value = FALSE))
+          radioButtons("radio", label = h3("Radio buttons"),
+                       choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                       selected = 1),
           
+          hr(),
+          textOutput("value")
+          #selectInput('plot', label = "Select Graph:", 
+         #             choices = list("Promoted Drugs" = 'promoChart', "Overall Drug Prescriptions" = 'drugSales',
+         #                            "Overall Drug Sales" = 'drugPrescriptions'),
+         #             selected = "promo"
+         # ),
+         # fluidRow(column(3, verbatimTextOutput("value"))),
+         # br()
+          #conditionalPanel(condition = "ouput.chart == 'sales'",
+          #                 checkboxInput("compareToData", label = "Compare to Drug Promotions", value = FALSE))
         ),
         mainPanel(
-          plotOutput("plot")
+          textOutput("text1"),
+          plotOutput("chart")
         )
       )
     ),
